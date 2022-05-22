b75926f006532c59fb31c41bb2885a8cb0e8d682
Igor Tron <root@itron.pro>
2022-05-22 15:57:52 +0200

    wp-config setup from .env

--- install/setup-wp.sh
+++ install/setup-wp.sh
@@ -2,18 +2,23 @@
 
 echo "WP setup preparing..."
 
+# import variables from .env file
+. ./.env
+
 # prepare file structure
 [ ! -d ./wp/ ] && mkdir -p ./wp/ && cp -r ./vendor/wordpress/wordpress/* ./wp
 [ ! -d ./wp-content/ ] && mkdir -p ./wp-content/
 cp -r ./vendor/wordpress/wordpress/wp-content/* ./wp-content
+
 [ ! -f ./index.php ] && echo "<?php
 // WordPress view bootstrapper
 define( 'WP_USE_THEMES', true );
 require( './wp/wp-blog-header.php' );" > index.php
-[ ! -f wp-config.php ] && cp ./install/.example/wp-config.php.example ./wp-config.php
 
-# import variables from .env file
-. ./.env
+if [ ! -f wp-config.php ]; then
+  WPCONFIG=$(< ./install/.example/wp-config.php.template)
+  printf "$WPCONFIG" $PROJECT_BASE_URL > ./wp-config.php
+fi
 
 # install WP
 echo "WP database init"

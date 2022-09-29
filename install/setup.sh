5d68d2dbe566b0f9b3f472a492a9548627d0126b
Igor Tron <root@itron.pro>
2022-09-29 09:55:53 +0200

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -10,5 +10,6 @@ echo "Containers created."
 echo "Composer install..."
 docker-compose exec php sh -c "composer install && bash ./install/setup-wp.sh"
 
-echo "Do not forget update the hosts file 127.0.0.1 devitron.loc"
+echo "Do not forget update the hosts file with line:"
+echo "127.0.0.1 devitron.loc"
 echo "Done."
